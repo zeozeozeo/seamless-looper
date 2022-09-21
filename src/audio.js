8bbe130d92fde9c7eb132d1e1d1d9a317c0dd818
zeozeozeo <zeocat9@gmail.com>
2022-09-21 20:13:22 +0500

    initial commit

---
+++ src/audio.js
@@ -0,0 +1,155 @@
+/*
+ * TODO: Crossfades, better loop recognition, optimization.
+ */
+
+if (typeof AudioContext == "function") {
+    var audioContext = new AudioContext();
+} else if (typeof webkitAudioContext == "function") {
+    // safari
+    var audioContext = new webkitAudioContext();
+}
+
+function Looper(data, file, onready, maxDiff = 5.5) {
+    this.blob = new window.Blob([new Uint8Array(data)]);
+
+    // convert the audio data into an audiobuffer
+    audioContext.decodeAudioData(
+        data,
+        (decoded) => {
+            this.sampleRate = decoded.sampleRate;
+            this.buffer = decoded.getChannelData(0);
+            var beatDetect = new BeatDetect({
+                sampleRate: this.sampleRate,
+                log: false, // Debug BeatDetect execution with logs
+                perf: false, // Attach elapsed time to result object
+                round: false, // To have an integer result for the BPM
+                float: 4, // The floating precision in [1, Infinity]
+                lowPassFreq: 150, // Low pass filter cut frequency
+                highPassFreq: 100, // High pass filter cut frequency
+                bpmRange: [90, 180], // The BPM range to output
+                timeSignature: 4, // The number of beat in a measure
+            });
+
+            // XXX: since BeatDetect.js only accepts URL's, we should create
+            // an object URL
+            const fileURL = window.URL.createObjectURL(file);
+            beatDetect
+                .getBeatInfo({
+                    url: fileURL,
+                })
+                .then((info) => {
+                    this.bpm = info.bpm;
+                    this.offset = info.offset;
+                    this.firstBar = info.firstBar;
+
+                    if (typeof onready == "function") {
+                        onready();
+                    }
+                })
+                .catch((err) => {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Error while detecting BPM!",
+                        text: err,
+                    }).then(() => {
+                        showFileChooseBox();
+                    });
+                    return;
+                });
+        },
+        (err) => {
+            Swal.fire({
+                icon: "error",
+                title: "Error while decoding audio data!",
+                text: err,
+            }).then(() => {
+                showFileChooseBox();
+            });
+            return;
+        }
+    );
+
+    this.analyze = () => {
+        // calculate the size of each chunk in samples:
+        // (60s / 120bpm) * 48000sr
+        const chunkSize = (60 / this.bpm) * this.sampleRate;
+        // sample number of the first beat
+        // const startSample = this.firstBar * this.sampleRate;
+        const startSample =
+            this.firstBar * this.sampleRate + this.offset * this.sampleRate;
+        const songLength = this.buffer.length - startSample;
+
+        // split audio into chunks
+        var chunks = [];
+        for (var i = startSample; i < this.buffer.length; i += chunkSize) {
+            const chunkData = this.buffer.slice(i, i + chunkSize);
+            chunks.push({
+                start: i,
+                end: i + chunkData.length,
+                data: chunkData,
+            });
+        }
+
+        // find loops
+        var loops = [];
+        for (var i = 0; i < chunks.length; i++) {
+            for (var j = 0; j < chunks.length && i !== j; j++) {
+                const chunk1 = chunks[i];
+                const chunk2 = chunks[j];
+                const length = Math.abs(chunk2.end - chunk1.start);
+                if (Math.abs(length - songLength) <= this.sampleRate) continue;
+                const diff = chunksDifference(chunk1.data, chunk2.data);
+
+                if (diff <= maxDiff) {
+                    const loopStart = chunk2.end.clamp(0, this.buffer.length);
+                    const loopEnd = chunk1.start.clamp(0, this.buffer.length);
+
+                    loops.push({
+                        start: loopStart,
+                        end: loopEnd,
+                        length: length,
+                        data: this.buffer.slice(loopStart, loopEnd),
+                        diff: diff,
+                    });
+                }
+            }
+        }
+        return sortLoops(loops, this.sampleRate);
+    };
+}
+
+// difference between two arrays
+function chunksDifference(a, b) {
+    var diff = 0;
+    for (var i = 0; i < a.length && i < b.length; i++) {
+        diff += Math.abs(a[i] - b[i]);
+    }
+    return diff;
+}
+
+function sortLoops(loops, sampleRate) {
+    return loops
+        .sort((a, b) => {
+            // sort loops by biggest to smallest
+            const length = b.length - a.length;
+            // if the length difference is smaller than 5 seconds,
+            // sort by best difference instead (TODO: check if this works)
+            if (Math.abs(length) < sampleRate * 5) {
+                return a.diff - b.diff;
+            }
+            return length;
+        })
+        .filter((loop) => {
+            for (var i = 0; i < loop.data.length; i++) {
+                // get rid of silent loops
+                if (loop.data[i] > 0.05) {
+                    return true;
+                }
+                return false;
+            }
+        });
+}
+
+Number.prototype.clamp = function (min, max) {
+    return Math.min(Math.max(this, min), max);
+};
---
+++ src/index.js
@@ -0,0 +1,170 @@
+/*
+ * TODO: Add the ability to select loops and the minimum difference threshold.
+ */
+
+var audioInput = document.getElementById("audio-input");
+var looper, wavesurfer, loops;
+
+var browseButton = document.getElementById("browse-button");
+var fileChooseBox = document.getElementById("file-choose-box");
+var loadingText = document.getElementById("loading-text");
+function showFileChooseBox() {
+    fileChooseBox.style.opacity = 1;
+    fileChooseBox.style.display = "block";
+    loadingText.style.display = "block";
+}
+showFileChooseBox();
+
+browseButton.addEventListener("click", () => {
+    var input = document.createElement("input");
+    input.type = "file";
+    input.click();
+
+    input.onchange = (event) => {
+        (function fade() {
+            (fileChooseBox.style.opacity -= 0.15) < 0
+                ? (fileChooseBox.style.display = "none")
+                : setTimeout(fade, 40);
+        })();
+        handleBrowse(event);
+    };
+});
+
+function handleBrowse(event) {
+    showPreview();
+    let reader = new FileReader();
+    reader.onload = (file) => {
+        if (typeof wavesurfer != "undefined") {
+            wavesurfer.destroy();
+        }
+        looper = new Looper(file.target.result, event.target.files[0], () => {
+            loops = looper.analyze();
+            if (loops.length === 0) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Couldn't find any loops!",
+                    text: "Try changing the minimum difference or try an another song.",
+                });
+            } else {
+                initUI();
+            }
+        });
+    };
+    reader.readAsArrayBuffer(event.target.files[0]);
+}
+
+function toSeconds(samples) {
+    if (typeof looper == "undefined" || looper.bufferLength == 0) return 0;
+    return samples / looper.sampleRate;
+}
+
+// blurred preview image
+const waveformPreview = document.getElementById("waveform-preview");
+function showPreview() {
+    waveformPreview.style.opacity = 1;
+    waveformPreview.style.display = "block";
+}
+function hidePreview() {
+    (function fade() {
+        (waveformPreview.style.opacity -= 0.1) < 0
+            ? (waveformPreview.style.display = "none")
+            : setTimeout(fade, 40);
+    })();
+}
+
+function initUI() {
+    const loopStart = toSeconds(loops[0].start);
+    const loopEnd = toSeconds(loops[0].end);
+
+    wavesurfer = WaveSurfer.create({
+        container: "#waveform",
+        waveColor: "#808080",
+        progressColor: "orange",
+        cursorColor: "white",
+        barWidth: 4,
+        partialRender: true,
+        responsive: true,
+        scrollParent: true,
+        height: window.innerHeight / 4,
+        normalize: true,
+        hideScrollbar: true,
+        autoCenter: true,
+        minPxPerSec: 70,
+        plugins: [
+            WaveSurfer.markers.create({
+                markers: [
+                    {
+                        time: loopStart,
+                        label: "Loop start",
+                        color: "#ff990a",
+                    },
+                    {
+                        time: loopEnd,
+                        label: "Loop end",
+                        color: "#00ffcc",
+                        position: "top",
+                    },
+                ],
+            }),
+        ],
+    });
+
+    wavesurfer.on("ready", () => {
+        hidePreview();
+        loadingText.style.display = "none";
+        wavesurfer.play(loopStart);
+    });
+
+    wavesurfer.on("audioprocess", (time) => {
+        if (time < loopStart || time >= loopEnd) {
+            if (wavesurfer.isPlaying()) {
+                wavesurfer.pause();
+            }
+            wavesurfer.play(loopStart);
+        }
+    });
+
+    wavesurfer.loadBlob(looper.blob);
+}
+
+document.getElementById("info-text").addEventListener("click", () => {
+    Swal.fire({
+        title: "Seamless Looper",
+        html: `
+        <ul>
+            <li>
+                Finds seamless loops in music.
+            </li>
+            <li>
+                Works best with electronic music.
+            </li>
+            <li>
+                You can interact with the waveform with your mouse.
+            </li>
+            <li>
+                The algorithm:
+                <ul>
+                    <li>
+                        1. Find BPM, starting beat and beat offset for the song.
+                    </li>
+                    <li>
+                        2. Split the song into chunks, the size of one chunk
+                        is the amount of samples between two beats.
+                    </li>
+                    <li>
+                        3. For each chunk, iterate through every other chunk
+                        and find their difference. If the difference is smaller than
+                        a certain threshold, skip it.
+                    </li>
+                    <li>
+                        4. Add the start and the end of the 2 matching chunks to an array of loops.
+                    </li>
+                    <li>
+                        5. Sort the array by biggest loops (not ideal, but that works).
+                    </li>
+                </ul>
+            </li>
+        </ul
+        `,
+    });
+});
